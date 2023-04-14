36cd4b7ac48441f8b5529c8b970db80d743975aa
Rahul7044 <rahulkumar581999@gmail.com>
2023-04-15 00:26:09 +0530

    uses NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 import "./App.css";
+import { Container, Navbar } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
+    <Navbar bg="dark" expand="sm" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">React Bootstrap</Navbar.Brand>
+      </Container>
+
+    </Navbar>
       
     </>
   );
