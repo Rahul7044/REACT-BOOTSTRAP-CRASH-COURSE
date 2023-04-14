4799c70449ed47ffdcd18b17650dadcf6d2bd49d
Rahul7044 <rahulkumar581999@gmail.com>
2023-04-14 23:50:19 +0530

    imported Button component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Button } from "react-bootstrap";
+import "./App.css";
 
 const App = () => {
   return (
-    < >
-    <h2>React Component</h2>
-      
+    <>
+      <h2>React Component</h2>
+      <Button variant="primary">Primary</Button>{" "}
+      <Button variant="secondary">Secondary</Button>{" "}
+      <Button variant="success">Success</Button>{" "}
+      <Button variant="warning">Warning</Button>{" "}
+      <Button variant="danger">Danger</Button>{" "}
+      <Button variant="info">Info</Button>{" "}
+      <Button variant="light">Light</Button>{" "}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
     </>
   );
-}
+};
 
 export default App;
